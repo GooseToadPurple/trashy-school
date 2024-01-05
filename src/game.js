2d1d9b4988d8ac7bd4816a9200948af58bd67e1d
GooseToadPurple <155771895+GooseToadPurple@users.noreply.github.com>
2024-01-05 18:00:57 -0500

    3 new

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ import wallImage from "../sprites/wall.png";
 import doorImage from "../sprites/door.png";
 import deskImage from "../sprites/desk.png";
 import chairImage from "../sprites/chair.png";
+import chairBlueImage from "../sprites/chair_blue.png";
 import chairSwivelImage from "../sprites/chair_swivel.png";
 import compImage from "../sprites/comp.png";
 import beanImage from "../sprites/bean.png";
@@ -12,11 +13,17 @@ import plImage from "../sprites/pennylee.png";
 import ssImage from "../sprites/sophiasmith.png";
 import rpImage from "../sprites/princessparis.png";
 import glImage from "../sprites/georgielyall.png";
+import llImage from "../sprites/lucylauren.png";
+import lgImage from "../sprites/lucygresty.png";
+import ajImage from "../sprites/axajay.png";
 
 import plVid from "../videos/plVid.mp4"; // https://spankbang.com/54tpn/video/tits
 import ssVid from "../videos/ssVid.mp4"; // https://spankbang.com/5l4j8/video/teachers+titties
 import rpVid from "../videos/rpVid.mp4"; // https://spankbang.com/5othu/video/i+love+being+s+slut
 import glVid from "../videos/glVid.mp4"; // https://spankbang.com/6c7dn/video/my+favourite+british+girl+joi
+import llVid from "../videos/llVid.mp4"; // https://spankbang.com/5hxsf-eh1qsc/playlist/downblouse+wankitnow
+import lgVid from "../videos/lgVid.mp4"; // https://spankbang.com/4h1z5/video/hot+for+teacher+joi
+import ajVid from "../videos/ajVid.mp4";// https://spankbang.com/3wgav-8f79dv/playlist/win+teacher
 
 
 kaboom({
@@ -27,6 +34,7 @@ loadSprite("wall", wallImage)
 loadSprite("carpet", carpetImage)
 loadSprite("desk", deskImage)
 loadSprite("chair", chairImage)
+loadSprite("chairBlue", chairBlueImage)
 loadSprite("chairSwivel", chairSwivelImage)
 loadSprite("comp", compImage)
 loadSprite("door", doorImage)
@@ -35,6 +43,9 @@ loadSprite("pennylee", plImage)
 loadSprite("sophiasmith", ssImage)
 loadSprite("princessparis", rpImage)
 loadSprite("georgielyall", glImage)
+loadSprite("lucylauren", llImage)
+loadSprite("lucygresty", lgImage)
+loadSprite("axajay", ajImage)
 
 let charismaPoints = 0;
 
@@ -53,7 +64,7 @@ scene("main", (levelIdx) => {
       "-0000000-",
       "-0000000-",
       "40000000-",
-      "-0000000-",
+      "-00000005",
       "-0000000-",
       "-0000000-",
       "-0000000-",
@@ -100,6 +111,30 @@ scene("main", (levelIdx) => {
       "-0000000-",
       "---------",
     ],
+    [
+      "---------------------",
+      "-0000000000000L000006",
+      "|00000000lvvv0000000-",
+      "-------------------7-",
+    ],
+    [
+      "---------------",
+      "-dd000Uw0000dd-",
+      "-00000ddd00000-",
+      "-00000u0c00000-",
+      "50000000000000-",
+      "---------------",
+    ],
+    [
+      "-------5-",
+      "-00A0000-",
+      "-00ddd00-",
+      "-00a0c00-",
+      "-0000000-",
+      "-0c0c0c0-",
+      "-0000000-",
+      "---------",
+    ],
   ]
 
   addLevel(levels[levelIdx], {
@@ -124,6 +159,11 @@ scene("main", (levelIdx) => {
         ],
       "c": () => [
         sprite("chair"),
+        area(),
+        "chair",
+        ],
+      "v": () => [
+        sprite("chairBlue"),
         area(),
         "chair",
         ],
@@ -213,6 +253,7 @@ scene("main", (levelIdx) => {
         { vid: plVid, timestamp1: 7, timestamp2: 286, thresh1: 40, irrMsg: "Get on with your work! (you need 40 charisma)", thresh2: 70 },
       ],
 
+
       "4": () => [
         sprite("door"),
         area(),
@@ -234,6 +275,70 @@ scene("main", (levelIdx) => {
         { vid: glVid, timestamp1: 5, timestamp2: 364, thresh1: 50, irrMsg: "Sorry, I'm too busy to help you. (you need 50 charisma)", thresh2: 80 },
       ],
 
+
+      "5": () => [
+        sprite("door"),
+        area(),
+        body({ isStatic: true }),
+        "door",
+        { idx: 5 },
+        ],
+      "L": () => [
+        sprite("lucylauren"),
+        area(),
+        body({ isStatic: true }),
+        "character",
+        { msg: "I'm sorry, but you have to go to detention." },
+      ],
+      "l": () => [
+        sprite("chairBlue"),
+        area(),
+        "special_chair",
+        { vid: llVid, timestamp1: 7, timestamp2: 365, thresh1: 80, irrMsg: "There you are! Detention is this way. (you need 80 charisma)", thresh2: 110 },
+      ],
+
+
+      "6": () => [
+        sprite("door"),
+        area(),
+        body({ isStatic: true }),
+        "door",
+        { idx: 6 },
+        ],
+      "U": () => [
+        sprite("lucygresty"),
+        area(),
+        body({ isStatic: true }),
+        "character",
+        { msg: "Take a seat and I will speak to you." },
+      ],
+      "u": () => [
+        sprite("chair"),
+        area(),
+        "special_chair",
+        { vid: lgVid, timestamp1: 4.5, timestamp2: 551, thresh1: 150, irrMsg: "Your grades are appalling. (you need 150 charisma)", thresh2: 180 },
+      ],
+
+      "7": () => [
+        sprite("door"),
+        area(),
+        body({ isStatic: true }),
+        "door",
+        { idx: 7 },
+        ],
+      "A": () => [
+        sprite("axajay"),
+        area(),
+        body({ isStatic: true }),
+        "character",
+        { msg: "Oh hello! You look like you're new." },
+      ],
+      "a": () => [
+        sprite("chair"),
+        area(),
+        "special_chair",
+        { vid: ajVid, timestamp1: 4.5, timestamp2: 478, thresh1: 9, irrMsg: "You should go to the headmistress. (you need 90 charisma)", thresh2: 12 },
+      ],
     }
   })
 
